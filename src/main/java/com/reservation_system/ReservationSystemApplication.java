1472dc90bf0573a8a2ffa5fbbd4aa5a5a5c23552
RJGY <rjgy2yu@gmail.com>
2021-10-18 00:10:44 +1100

    smol commit

--- src/main/java/com/reservation_system/ReservationSystemApplication.java
+++ src/main/java/com/reservation_system/ReservationSystemApplication.java
@@ -2,7 +2,15 @@ package com.reservation_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 @SpringBootApplication
 public class ReservationSystemApplication {
@@ -11,4 +19,23 @@ public class ReservationSystemApplication {
         SpringApplication.run(ReservationSystemApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner loadData(UserRepository userRepository,
+                                      ReservationRepository reservationRepository) {
+        return (args) -> {
+            User user = userRepository.save(new User());
+            DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+            Date date = new Date();
+            LocalDate localDate = date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+            Reservation reservation = Reservation.builder()
+                    .reservationDate(localDate)
+                    .startTime(LocalTime.of(12, 00))
+                    .endTime(LocalTime.of(13, 00))
+                    .user(user)
+                    .amenityType(AmenityType.POOL)
+                    .build();
+
+            reservationRepository.save(reservation);
+        };
+    }
 }
